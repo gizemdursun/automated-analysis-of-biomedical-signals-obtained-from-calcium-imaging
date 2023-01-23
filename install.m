ef60bd8e1669c051a87b395dd7367f508189d09f
Gizem DURSUN DEMİR <gizemdursun91@gmail.com>
2023-01-23 15:19:21 +0300

    Update install.m

--- install.m
+++ install.m
@@ -1,6 +1,7 @@
 function [sROI,numROI,video,imgstk] = install()
 
 % load calcium imaging video data
+% you can download .stk data from the link. (https://drive.google.com/file/d/1mZyr9v3jlzoGX8fRKKc_LsXr4K06COS3/view?usp=share_link)
 [filename,pathname] = uigetfile({'*.stk'},'Open TIFF file with imaging video', 'MultiSelect', 'off');
 fileVideo=fullfile(pathname,filename);
 
